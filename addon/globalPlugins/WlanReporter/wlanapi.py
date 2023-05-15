da7445197effd31e3945393890bd86d8b41c9561
Kvark <kvark128@yandex.ru>
2023-05-15 08:01:59 +0800

    Some improvements and additions to the wlanapi.py module

--- addon/globalPlugins/WlanReporter/wlanapi.py
+++ addon/globalPlugins/WlanReporter/wlanapi.py
@@ -15,10 +15,19 @@ wlan_notification_acm_disconnected = 0x00000015
 wlan_notification_acm_interface_arrival = 0x0000000d
 wlan_notification_acm_interface_removal = 0x0000000e
 
+# Various flags for the network
 WLAN_AVAILABLE_NETWORK_CONNECTED = 1
+WLAN_AVAILABLE_NETWORK_HAS_PROFILE = 2
 
-# State of the interface
+# States of an interface
+wlan_interface_state_not_ready = 0
 wlan_interface_state_connected = 1
+wlan_interface_state_ad_hoc_network_formed = 2
+wlan_interface_state_disconnecting = 3
+wlan_interface_state_disconnected = 4
+wlan_interface_state_associating = 5
+wlan_interface_state_discovering = 6
+wlan_interface_state_authenticating = 7
 
 # General reason codes
 WLAN_REASON_CODE_SUCCESS = 0
@@ -38,6 +47,11 @@ DOT11_AUTH_ALGO_WPA_PSK = 4
 DOT11_AUTH_ALGO_WPA_NONE = 5
 DOT11_AUTH_ALGO_RSNA = 6
 DOT11_AUTH_ALGO_RSNA_PSK = 7
+DOT11_AUTH_ALGO_WPA3 = 8
+DOT11_AUTH_ALGO_WPA3_ENT_192 = DOT11_AUTH_ALGO_WPA3
+DOT11_AUTH_ALGO_WPA3_SAE = 9
+DOT11_AUTH_ALGO_OWE = 10
+DOT11_AUTH_ALGO_WPA3_ENT = 11
 DOT11_AUTH_ALGO_IHV_START = 0x80000000
 DOT11_AUTH_ALGO_IHV_END = 0xffffffff
 
@@ -108,6 +122,7 @@ class WLAN_INTERFACE_INFO_LIST(Structure):
 		("InterfaceInfo", WLAN_INTERFACE_INFO * 1),
 	]
 
+# Type of notification callback function
 WLAN_NOTIFICATION_CALLBACK = CFUNCTYPE(None, POINTER(WLAN_NOTIFICATION_DATA), POINTER(c_void_p))
 
 def errcheck(result, func, args):
@@ -116,44 +131,44 @@ def errcheck(result, func, args):
 	return result
 
 # Function prototypes from wlanapi.dll
-wlanapi = windll.wlanapi
-wlanapi.WlanOpenHandle.errcheck = errcheck
-wlanapi.WlanOpenHandle.argtypes = [DWORD, c_void_p, POINTER(DWORD), POINTER(HANDLE)]
-wlanapi.WlanOpenHandle.restype = DWORD
-wlanapi.WlanEnumInterfaces.errcheck = errcheck
-wlanapi.WlanEnumInterfaces.argtypes = [HANDLE, c_void_p, POINTER(POINTER(WLAN_INTERFACE_INFO_LIST))]
-wlanapi.WlanEnumInterfaces.restype = DWORD
-wlanapi.WlanGetAvailableNetworkList.errcheck = errcheck
-wlanapi.WlanGetAvailableNetworkList.argtypes = [HANDLE, POINTER(GUID), DWORD, c_void_p, POINTER(POINTER(WLAN_AVAILABLE_NETWORK_LIST))]
-wlanapi.WlanGetAvailableNetworkList.restype = DWORD
-wlanapi.WlanFreeMemory.argtypes = [c_void_p]
-wlanapi.WlanCloseHandle.errcheck = errcheck
-wlanapi.WlanCloseHandle.argtypes = [HANDLE, c_void_p]
-wlanapi.WlanCloseHandle.restype = DWORD
-wlanapi.WlanRegisterNotification.errcheck = errcheck
-wlanapi.WlanRegisterNotification.argtypes = [HANDLE, DWORD, BOOL, WLAN_NOTIFICATION_CALLBACK, c_void_p, c_void_p, PDWORD]
-wlanapi.WlanRegisterNotification.restype = DWORD
+_wlanapi_dll = windll.wlanapi
+_wlanapi_dll.WlanOpenHandle.errcheck = errcheck
+_wlanapi_dll.WlanOpenHandle.argtypes = [DWORD, c_void_p, POINTER(DWORD), POINTER(HANDLE)]
+_wlanapi_dll.WlanOpenHandle.restype = DWORD
+_wlanapi_dll.WlanEnumInterfaces.errcheck = errcheck
+_wlanapi_dll.WlanEnumInterfaces.argtypes = [HANDLE, c_void_p, POINTER(POINTER(WLAN_INTERFACE_INFO_LIST))]
+_wlanapi_dll.WlanEnumInterfaces.restype = DWORD
+_wlanapi_dll.WlanGetAvailableNetworkList.errcheck = errcheck
+_wlanapi_dll.WlanGetAvailableNetworkList.argtypes = [HANDLE, POINTER(GUID), DWORD, c_void_p, POINTER(POINTER(WLAN_AVAILABLE_NETWORK_LIST))]
+_wlanapi_dll.WlanGetAvailableNetworkList.restype = DWORD
+_wlanapi_dll.WlanFreeMemory.argtypes = [c_void_p]
+_wlanapi_dll.WlanCloseHandle.errcheck = errcheck
+_wlanapi_dll.WlanCloseHandle.argtypes = [HANDLE, c_void_p]
+_wlanapi_dll.WlanCloseHandle.restype = DWORD
+_wlanapi_dll.WlanRegisterNotification.errcheck = errcheck
+_wlanapi_dll.WlanRegisterNotification.argtypes = [HANDLE, DWORD, BOOL, WLAN_NOTIFICATION_CALLBACK, c_void_p, c_void_p, PDWORD]
+_wlanapi_dll.WlanRegisterNotification.restype = DWORD
 
 def WlanOpenHandle(dwClientVersion, pReserved, pdwNegotiatedVersion, phClientHandle):
 	""" The WlanOpenHandle function opens a connection to the server. """
-	return wlanapi.WlanOpenHandle(dwClientVersion, pReserved, pdwNegotiatedVersion, phClientHandle)
+	return _wlanapi_dll.WlanOpenHandle(dwClientVersion, pReserved, pdwNegotiatedVersion, phClientHandle)
 
 def WlanEnumInterfaces(hClientHandle, pReserved, ppInterfaceList):
 	""" The WlanEnumInterfaces function enumerates all of the wireless LAN interfaces currently enabled on the local computer. """
-	return wlanapi.WlanEnumInterfaces(hClientHandle, pReserved, ppInterfaceList)
+	return _wlanapi_dll.WlanEnumInterfaces(hClientHandle, pReserved, ppInterfaceList)
 
 def WlanGetAvailableNetworkList(hClientHandle, pInterfaceGuid, dwFlags, pReserved, ppAvailableNetworkList):
 	""" The WlanGetAvailableNetworkList function retrieves the list of available networks on a wireless LAN interface. """
-	return wlanapi.WlanGetAvailableNetworkList(hClientHandle, pInterfaceGuid, dwFlags, pReserved, ppAvailableNetworkList)
+	return _wlanapi_dll.WlanGetAvailableNetworkList(hClientHandle, pInterfaceGuid, dwFlags, pReserved, ppAvailableNetworkList)
 
 def WlanFreeMemory(pMemory):
 	""" The WlanFreeMemory function frees memory. Any memory returned from Native Wifi functions must be freed. """
-	wlanapi.WlanFreeMemory(pMemory)
+	_wlanapi_dll.WlanFreeMemory(pMemory)
 
 def WlanCloseHandle(hClientHandle, pReserved):
 	""" The WlanCloseHandle function closes a connection to the server. """
-	return wlanapi.WlanCloseHandle(hClientHandle, pReserved)
+	return _wlanapi_dll.WlanCloseHandle(hClientHandle, pReserved)
 
 def WlanRegisterNotification(hClientHandle, dwNotifSource, bIgnoreDuplicate, funcCallback, pCallbackContext, pReserved, pdwPrevNotifSource):
 	""" The WlanRegisterNotification function is used to register and unregister notifications on all wireless interfaces. """
-	return wlanapi.WlanRegisterNotification(hClientHandle, dwNotifSource, bIgnoreDuplicate, funcCallback, pCallbackContext, pReserved, pdwPrevNotifSource)
+	return _wlanapi_dll.WlanRegisterNotification(hClientHandle, dwNotifSource, bIgnoreDuplicate, funcCallback, pCallbackContext, pReserved, pdwPrevNotifSource)
